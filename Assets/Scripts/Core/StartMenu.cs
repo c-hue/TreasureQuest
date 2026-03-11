ac0436151fd40fbc65d76e3e3c5148a0566e54d2
c-hue <caitlyndhue@gmail.com>
2026-03-11 15:19:21 -0700

    start menu and how to play menu

---
+++ Assets/Scripts/Core/StartMenu.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadSceneAsync(1);
+    }
+
+    public void BackToMenu()
+    {
+        SceneManager.LoadSceneAsync(0);
+    }
+
+    public void HowToPlay()
+    {
+        SceneManager.LoadSceneAsync(2);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    
+}
